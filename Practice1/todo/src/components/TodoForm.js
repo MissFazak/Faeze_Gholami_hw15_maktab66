6e6dd5b28fa9de8a951ab661b724302fed13ad8c
Faeze Gholami <48484129+MissFazak@users.noreply.github.com>
2022-03-12 17:00:16 +0330

    Update TodoForm.js

--- Practice1/todo/src/components/TodoForm.js
+++ Practice1/todo/src/components/TodoForm.js
@@ -1,63 +1,15 @@
-import React, { useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
+import React, { Component } from 'react'
 
-const TodoForm = ({
-  input,
-  setInput,
-  todos,
-  setTodos,
-  editTodo,
-  setEditTodo,
-}) => {
-  const updateTodo = (title, id, completed) => {
-    const newTodo = todos.map((todo) =>
-      todo.id === id ? {title, id, completed }: todo
-    );
-    setTodos(newTodo);
-    setEditTodo("");
-  };
-
-  useEffect(() => {
-    if (editTodo) {
-      setInput(editTodo.title);
-    } else {
-      setInput("");
-    }
-  }, [setInput, editTodo]);
-  const onInputChange = (event) => {
-    setInput(event.target.value);
-  };
-
-  const onFormSubmit = (event) => {
-    event.preventDefault();
-    if (!editTodo) {
-      setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
-      setInput("");
-    } else {
-      updateTodo(input, editTodo.id, editTodo.completed);
-    }
-  };
-  return (
-    <div className="header">
-      <h1>تودو لیست</h1>
-      <form onSubmit={onFormSubmit}>
-        <input
-          type="text"
-          placeholder="تسک جدید وارد کنید"
-          className="text-input"
-          value={input}
-          required
-          onChange={onInputChange}
-        />
-        <button className="button-add" type="submit">
-          {editTodo ? "ویرایش" : "جدید"}
-        </button>
-      </form>
-    </div>
-  );
-};
-
-export default TodoForm;
-
-
-
+export default class TodoForm extends Component {
+  render() {
+    const {item, handleChange,handleSubmit,editItem} = this.props
+    return (
+      <div className="header">
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder='تسک جدید وارد کنید...'  value={item} onChange={handleChange} className="text-input"/>
+          <button type='submit' className="button-add">{editItem?'ویرایش':'اضافه'}</button>
+        </form>
+      </div>
+    )
+  }
+}
