f40df63b4994a0386253f91e5874ed0d819ae127
Faeze Gholami <48484129+MissFazak@users.noreply.github.com>
2022-03-12 17:00:42 +0330

    Update TodoItem.js

--- Practice1/todo/src/components/TodoItem.js
+++ Practice1/todo/src/components/TodoItem.js
@@ -1,60 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import { RiCloseCircleLine } from "react-icons/ri";
-import { BiCheckCircle, BiEditAlt } from "react-icons/bi";
+import { BiEditAlt } from "react-icons/bi";
 
-const TodoItem = ({ todos, setTodos, setEditTodo }) => {
-  const handleComplete = (todo) => {
-    setTodos(
-      todos.map((item) => {
-        if (item.id === todo.id) {
-          return { ...item, completed: !item.completed };
-        }
-        return item;
-      })
+export default class extends Component {
+  render() {
+    const { items ,handleDelete ,handleEdit} = this.props;
+    return (
+      <div>
+        {items.map((item) => {
+          return (
+            <li key={item.id} className="list-item">
+              <input value={item.title} className="list"/>
+              <div>
+                <button onClick={()=>handleEdit(item.id)} className="button-edit task-button">
+                <BiEditAlt/>
+                </button>
+                <button onClick={()=>handleDelete(item.id)} className="button-delete task-button">
+                <RiCloseCircleLine />
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      </div>
     );
-  };
-  
-  const handleEdit = ({ id }) => {
-    const findTodo = todos.find((todo) => todo.id === id);
-    setEditTodo(findTodo);
-  };
-  const handleDelete = ({ id }) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-  return (
-    <div>
-      {todos.map((todo) => (
-        <li className="list-item" key={todo.id}>
-          <input
-            type="text"
-            value={todo.title}
-            className={`list ${todo.completed ? "complete" : ""}`}
-            onChange={(event) => event.preventDefault()}
-          />
-          <div>
-            <button
-              className="button-complete task-button"
-              onClick={() => handleComplete(todo)}
-            >
-              <BiCheckCircle />
-            </button>
-            <button
-              className="button-edit task-button"
-              onClick={() => handleEdit(todo)}
-            >
-              <BiEditAlt />
-            </button>
-            <button
-              className="button-delete task-button"
-              onClick={() => handleDelete(todo)}
-            >
-              <RiCloseCircleLine />
-            </button>
-          </div>
-        </li>
-      ))}
-    </div>
-  );
-};
-
-export default TodoItem;
+  }
+}
